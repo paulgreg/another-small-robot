9a1571317c35891dfbd17fb84bcec05544700b72
Grégory Paul <paulgreg@gmail.com>
2020-04-22 19:38:35 +0200

    WIP

--- main/main.ino
+++ main/main.ino
@@ -1,17 +1,37 @@
-#define DEBUG false
-
-#define SAFE_DISTANCE 12
-#define TURNING_FOR 8
-
-#define SIWTCH_PIN 13
-
-#define trigger 12
-#define echo 11
-
-int turnCounter = 0;
+#define SWITCH_PIN 13
 
 int motorEnablePin[] = {9, 10};
 int motorPins[] = {6, 8, 5, 7};
+
+char F = 'F';
+char R = 'R';
+char L = 'L';
+char S = 'S';
+
+int stepIdx = 0;
+int delayUntilEndStep = 0;
+
+char steps[] = {
+  S,
+  F,
+  F,
+  F,
+  F,
+  F,
+  F,
+  F,
+  R,
+  F,
+  F,
+  F,
+  F,
+  R,
+  F,
+  F
+};
+
+int stepNb = 5;
+
 
 void setup() {
   pinMode(motorEnablePin[0], OUTPUT);
@@ -20,52 +40,8 @@ void setup() {
   pinMode(motorPins[1], OUTPUT);
   pinMode(motorPins[2], OUTPUT);
   pinMode(motorPins[3], OUTPUT);
-
-  pinMode(SIWTCH_PIN, INPUT);
-
-  pinMode(trigger, OUTPUT);
-  digitalWrite(trigger, LOW);
-  pinMode(echo, INPUT);
-  if (DEBUG) Serial.begin(9600);
-}
-
-void loop() {
-  if (digitalRead(SIWTCH_PIN) == LOW) {
-    stop();
-    delay(1000);
-  } else {
-    if (turnCounter > 0) {
-      if (DEBUG) Serial.println("turning");
-      turnRight();
-      turnCounter--;
-    } else {
-      long distanceInCm = getDistanceInCm();
-      if (DEBUG) Serial.println(distanceInCm);
-      
-      if (distanceInCm > SAFE_DISTANCE) {
-        if (DEBUG) Serial.println("forward");
-        goForward();
-      } else {
-        turnCounter = TURNING_FOR;
-      }
-    }
-    delay(250);
-  }
-}
-
-void stop() {
-  setMotor(0, 0, false);
-  setMotor(1, 0, false);
-}
-
-void turnRight() {
-  setMotor(0, 255, false);
-  setMotor(1, 255, true);
-}
-
-void goForward() {
-  setMotor(0, 255, false);
-  setMotor(1, 255, false);
+  pinMode(SWITCH_PIN, INPUT);
+  Serial.begin(9600);
 }
 
 void setMotor(int idx, int speed, boolean reverse) {
@@ -74,11 +50,65 @@ void setMotor(int idx, int speed, boolean reverse) {
   digitalWrite(motorPins[idx + 2], reverse);
 }
 
-long getDistanceInCm() {
-  digitalWrite(trigger, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger, LOW);
-  long echoRead = pulseIn(echo, HIGH);
-  return echoRead / 58;
+void turnLeft() {
+  Serial.println("left");
+  setMotor(0, 255, true);
+  setMotor(1, 255, false);
+  delayUntilEndStep = 3400;
 }
 
+void turnRight() {
+  Serial.println("right");
+  setMotor(0, 255, false);
+  setMotor(1, 255, true);
+  delayUntilEndStep = 3400;
+}
+
+void goForward() {
+  Serial.println("forward");
+  setMotor(0, 255, false);
+  setMotor(1, 255, false);
+  delayUntilEndStep = 3000;
+}
+
+void stop() {
+  Serial.println("stop");
+  setMotor(0, 0, false);
+  setMotor(1, 0, false);
+  delayUntilEndStep = 1000;
+}
+
+void end() {
+  while(true) {
+    Serial.println("end");
+    delay(1000);
+  }
+}
+
+void loop() {
+  if (digitalRead(SWITCH_PIN) == LOW) {
+    if (delayUntilEndStep > 0) delayUntilEndStep -= 100;
+    if (delayUntilEndStep == 0) {
+      stepIdx++;
+      if (stepIdx > stepNb) end();
+      Serial.print("Next step: ");
+      Serial.println(steps[stepIdx]);
+      switch (steps[stepIdx]) {
+        case 'F':
+          goForward();
+          break;
+        case 'R':
+          turnRight();
+          break;
+        case 'L':
+          turnRight();
+          break;
+        default:
+          stop();
+      }
+    }
+  } else {
+    stop();
+  }
+  delay(100);
+}
